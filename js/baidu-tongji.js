f8e872e4dfa0623ed033fb1ad174469122d03e90
balconychy <caihaiyang1986@gmail.com>
2023-04-07 14:33:03 +0800

    Site updated: 2023-04-07 14:33:03

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?";
+  hm.src = "https://hm.baidu.com/hm.js?81f6ff727359441438b5c831609c3118";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
